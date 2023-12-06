7b5dd06cd72015d6eb6d9f0a4dc958c08a818711
sagesteppe <reedbenkendorf27@gmail.com>
2023-12-06 14:26:55 -0700

    draft absence pseudo abs pt generator

--- scripts/absence_drawer.R
+++ scripts/absence_drawer.R
@@ -24,37 +24,54 @@ absence_drawer <- function(x, bg_abs){
   box_hull <- sf::st_difference(bbox, buffer_hull)
   
   # perform the outline background absence sample
-  reg_pts <- sf::st_sample(box_hull, size = round(nrow(x) * bg_abs, 0), type = 'regular')
+  reg_pts <- sf::st_sample(box_hull, size = round(nrow(x) * bg_abs, 0), type = 'regular') |>
+    sf::st_as_sf() |>
+    dplyr::mutate(Occurrence = 0, PtType = 'Background Absence') |>
+    dplyr::rename(geometry = x)
   
-  rm(box_hull, bbox, observed_hull)
+  rm(bbox, observed_hull)
   
-
   # identify AIM plots and generate absences. 
   
   blm_surf_area <- as.numeric(sf::st_area(blm_surf))
-  prcnt_blm <- (blm_surf_area / buf_area) * 100
-  aim_recs <- (1 - bg_abs) * round(prcnt_blm, 0) * 0.1
+  prcnt_blm <- (blm_surf_area / as.numeric(sf::st_area(buffer_hull))) 
+  aim_recs <- nrow(x) * (1 - bg_abs) * prcnt_blm
   
   AIM_points <- AIM_points[lengths(st_intersects(AIM_points, buffer_hull)) > 0,]
   AIM_points <- AIM_points |> 
     filter(! PrimaryKey %in% x$PrimaryKey ) # these are presences
   
-  # generate AIM plot absences
+  if(aim_recs >= nrow(AIM_points)){aim_recs <- nrow(AIM_points)} # in case species v. abundant. 
   
-  
-#  st_intersection(aim_plots, blm_admin_surf)
-  
-  
+  AIM_absence <- sample_n(AIM_points, size = round(aim_recs * 1.1, 0), replace = FALSE) |> 
+    dplyr::mutate(Occurrence = 0, PtType = 'AIM Absence') |>
+    dplyr::select(Occurrence, PtType, geometry) |>
+    sf::st_transform(sf::st_crs(x))
+
   # generate Random absences
   
-#  rand_recs <- ((1 - bg_abs) * (1 - prcnt_blm)) * 0.1
-#  buffered_presences <- sf::st_buffer(x, dist = 10000) |>
-#    sf::st_union()
+  rand_recs <- nrow(x) * ((1 - bg_abs) * (1 - prcnt_blm))
+  buffered_presences <- sf::st_buffer(x, dist = 10000) |>
+    sf::st_union() |>
+    sf::st_transform(sf::st_crs(blm_surf))
   
-#  non_blm_area <- sf::st_difference(blm_admin_surf, buffer_hull) 
-#  non_blm_area <- sf::st_difference(non_blm_area, buffered_presences)
+  buffer_hull <- sf::st_transform(buffer_hull, sf::st_crs(blm_surf)) |>
+    sf::st_make_valid()
+  non_blm_area <- sf::st_difference(buffer_hull, blm_surf) 
+  non_blm_area <- sf::st_difference(non_blm_area, buffered_presences)
   
-#  random_samples <- sf::st_sample(non_blm_area, prcnt_blm)
+  random_samples <- sf::st_sample(non_blm_area, rand_recs, 
+                                  size = round(rand_recs * 1.1, 0),
+                                  type = 'random') |> 
+    sf::st_as_sf() |>
+    dplyr::mutate(Occurrence = 0, PtType = 'Random Absence') |>
+    sf::st_transform(sf::st_crs(x)) |>
+    dplyr::rename(geometry = x)
+  
+  absences <- bind_rows(reg_pts, AIM_absence, random_samples)
+  
+  return(absences)
   
 }
+
 
