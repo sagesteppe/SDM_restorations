f4edd300852dd41d0eb2cdfee27071b2f3ac9ca1
sagesteppe <reedbenkendorf27@gmail.com>
2023-12-06 13:26:05 -0700

    flesh fn

--- scripts/absence_drawer.R
+++ scripts/absence_drawer.R
@@ -5,27 +5,39 @@ absence_drawer <- function(x, bg_abs){
   # and a hull to form the basis for a bounding box to generate regular absences in
   
   observed_hull <- sf::st_union(x) |>
-    sf::st_convex_hull()
+    sf::st_convex_hull() |>
+    sf::st_sfc() 
   
-  obs_area <- sf::st_area(observed_hull)
-  buffer_hull <- st_buffer(observed_hull, dist = (obs_area*0.25)/2  ) 
-  buf_area <- sf::st_area(buffer_hull)
-  box_hull <- sf::st_buffer(buffer_hull, dist = (buf_area*0.1)/2)
+  obs_area <- as.numeric(st_area(observed_hull)) * 0.15 / 2
+  obs_area <- if(obs_area > 160934){obs_area = 160934} # cap area with 100 mi buffer. 
+  
+  buffer_hull <- st_buffer(observed_hull, obs_area) 
+  
+  buf_area <- as.numeric(sf::st_area(buffer_hull) *0.1) / 2
+  obs_area <- if(buf_area > 40233){buf_area = 40233} # cap area with 25 mi buffer.
+  
+  box_hull <- sf::st_buffer(buffer_hull, dist = obs_area)
   
   bbox <- sf::st_bbox(box_hull) |>
     sf::st_as_sfc() 
   
-  box_hull <- sf::st_difference(buffer_hull, box_hull)
+  box_hull <- sf::st_difference(bbox, buffer_hull)
   
   # perform the outline background absence sample
   reg_pts <- sf::st_sample(box_hull, size = round(nrow(x) * bg_abs, 0), type = 'regular')
   
+  rm(box_hull, bbox, observed_hull)
+  
+
   # identify AIM plots and generate absences. 
   
-  blm_surf_area <- sf::st_area(blm_surf)
+  blm_surf_area <- as.numeric(sf::st_area(blm_surf))
   prcnt_blm <- (blm_surf_area / buf_area) * 100
-
   aim_recs <- (1 - bg_abs) * round(prcnt_blm, 0) * 0.1
+  
+  AIM_points <- AIM_points[lengths(st_intersects(AIM_points, buffer_hull)) > 0,]
+  AIM_points <- AIM_points |> 
+    filter(! PrimaryKey %in% x$PrimaryKey ) # these are presences
   
   # generate AIM plot absences
   
